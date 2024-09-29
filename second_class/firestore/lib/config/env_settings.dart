a46077bd5149cc56eabb8d57b566e45d94b0a7b7
Martín Doti <mdoti92@gmail.com>
2024-09-29 20:17:40 -0300

    environment variables added

---
+++ second_class/firestore/lib/config/env_settings.dart
@@ -0,0 +1,10 @@
+class EnvSettings {
+  static const String androidApiKey = 'ANDROID_API_KEY';
+  static const String androidAppId = 'APP_ID';
+  static const String messagingSenderId = 'MESSAGING_SENDER_ID';
+  static const String projectId = 'PROJECT_ID';
+  static const String storageBucket = 'STORAGE_BUCKET';
+  static const String iosApiKey = 'IOS_API_KEY';
+  static const String iosAppId = 'IOS_APP_ID';
+  static const String iosBundleId = 'IOS_BUNDLE_ID';
+}
--- second_class/firestore/lib/main.dart
+++ second_class/firestore/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firestore/firebase_options.dart';
 import 'package:firestore/screens/second_class_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
